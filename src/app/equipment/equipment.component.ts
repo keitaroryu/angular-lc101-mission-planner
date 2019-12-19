540159ff4ad6215cde72bee45318320ff3cc976b
keitaroryu <keitaroryu@gmail.com>
2019-12-18 18:58:11 -0600

    Finished Equipment Component for Angular Part 3 Studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-equipment',
@@ -6,26 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+  equipmentItems: object[] = [
+      {name: 'Duct Tape', mass: 0.5},
+      {name: 'Space Camera', mass: 20},
+      {name: 'Food', mass: 150},
+      {name: 'Oxygen Tanks', mass: 400},
+      {name: 'AE-35 Unit', mass: 5},
+      {name: 'ISS Supplies', mass: 800},
+      {name: 'Water', mass: 250},
+      {name: 'Satellite', mass: 1200},
+      {name: 'R2 Unit', mass: 32}
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
+  addCargoColor: string = 'gold';
+  remainingMass: number = this.maximumAllowedMass;
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() { }
+  ngOnInit() { }
 
-   // Code your addItem function here:
-   
+  // Code your addItem function here:
+  addItem(item: object){
+    this.cargoHold.push(item);
+    this.cargoMass += item['mass'];
+    this.remainingMass -= item['mass'];
+    if(this.cargoMass > this.maximumAllowedMass){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  disableAddCargoButton(item:object){
+    if(this.cargoHold.length===this.maxItems || (this.cargoMass + item['mass'])>this.maximumAllowedMass){
+      return true;
+    }
+  }
 }
